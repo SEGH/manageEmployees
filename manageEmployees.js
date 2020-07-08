c11fbc25be3873864eed0ebfafee449cb29ecc91
Susan Holland <segh@fastmail.com>
2020-07-08 19:27:05 -0400

    Pseudocode

---
+++ manageEmployees.js
@@ -0,0 +1,18 @@
+// Dependencies
+
+// Database Connection
+    // Call function that runs main inquirer prompts
+
+// Prompt Question Arrays
+
+// Function to run main inquirer prompts (choice list)
+    // If Exit is chosen, end connection
+
+// Function to run prompts needed to add departments, roles, and employees
+    // Call function to re-run main inquirer prompts again at end
+
+// Function to run prompts needed to view departments, roles, and employees
+    // Call function to re-run main inquirer prompts again at end
+
+// Function to run prompts needed to update employee roles
+    // Call function to re-run main inquirer prompts again at end
